9df648a86914942ba35dc38b616c4ce6abbb36fd
kimgech <kimgech309@gmail.com>
2022-09-14 16:36:56 +0700

    Initial commit

---
+++ src/main/kotlin/com/example/webfluxdemo/config/SecurityConfig.kt
@@ -0,0 +1,26 @@
+package com.example.webfluxdemo.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.config.web.server.invoke
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    fun filterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        return http.invoke{
+            cors{}
+            csrf{ disable() }
+            authorizeExchange{
+                authorize("/api/v1/instance**", authenticated)
+                authorize("/api/v1/os**", permitAll)
+            }
+            oauth2ResourceServer {
+                jwt {}
+            }
+        }
+    }
+}
